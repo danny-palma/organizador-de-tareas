f790f8b8941c1ca7f27710657e3436ce4ff6588c
hola-sebas <danielpalma2003@hotmail.com>
2020-09-09 12:19:09 -0500

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path')
 const app = express()
-const port = '3000'
+
 app.set('port', 3000)
 
 app.get('/', (req, res) => {
-    res.sendFile('public/index.html')
+    res.sendFile(path.join(__dirname, 'public/index.html' ))
 })
 
 app.listen(app.get('port'), () => {
