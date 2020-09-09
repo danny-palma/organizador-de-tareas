17612b3679962f55279a51651a4817d4bc51d349
hola-sebas <danielpalma2003@hotmail.com>
2020-09-09 14:15:29 -0500

    avr si esto jala asi

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ app.set('views', path.join(__dirname, 'public'))
 app.set('view engine', 'html')
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.get('/', (req, res) => {
-    res.sendFile('index')
-})
 
 app.listen(app.get('port'), () => {
     console.log(`server en el puerto ${app.get('port')}`)
