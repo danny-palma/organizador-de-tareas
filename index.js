b709a3199802a97696431cbc05c08282110238d6
hola-sebas <danielpalma2003@hotmail.com>
2020-09-09 14:10:48 -0500

    nice

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path')
 const app = express()
 
-app.set('port', 3000)
+app.set('port', process.env.PORT || 3000)
 app.set('views', path.join(__dirname, 'public'))
 app.set('view engine', 'html')
 
