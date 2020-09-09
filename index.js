4c00346e348245ac4b6d0140dfba53d128ee58d7
hola-sebas <danielpalma2003@hotmail.com>
2020-09-09 12:10:58 -0500

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express()
 const port = '3000'
+app.set('port', 3000)
 
 app.get('/', (req, res) => {
-    res.render('public')
+    res.send(require('public/index.html'))
 })
 
-app.listen(port, () => {
-    console.log(`server en el puerto ${port}`)
+app.listen(app.get('port'), () => {
+    console.log(`server en el puerto ${app.get('port')}`)
 })
